f847f7385f076201f1f06d31ebaedbd1fec57bd1
macogil <macogil@gmail.com>
2017-04-08 20:14:02 +0200

    add dotenv module

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var express = require ('express');
 var path = require ('path');
 var mongoose = require ('mongoose');
 var validUrl = require('valid-url');
+require('dotenv').config({
+  silent: true
+});
 
 var app = express();
 
 // Setup and connect to mongoDB
 var mongourl = process.env.MONGODB_URI || 'mongodb://admin:admin@ds113670.mlab.com:13670/shorturls';
+mongoose.Promise = global.Promise;
 mongoose.connect(mongourl);
 
 // Get database model
@@ -20,7 +24,7 @@ app.get('/', function(req, res){
 // Access a shrotened URL
 app.get('/:urlToForward', function(req, res){
   var urlToForward = req.params.urlToForward;
-  console.log(urlToForward);
+  //console.log(urlToForward);
   shortUrl.findOne({ 'shorterUrl': urlToForward }, function(err, data){
     if (err) return res.send('Error reading database');
     var reg = new RegExp("^(http||https)://","i");
